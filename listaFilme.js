5b57a8d2445e85a22ebf84effc13d6f686cdaf8b
SupremeAlienCode <supremealiencode.contato@gmail.com>
2025-11-22 20:24:30 -0300

    Add files via upload

---
+++ listaFilme.js
@@ -0,0 +1,22 @@
+let lista = {
+      id1: {
+        foto: "https://img.youtube.com/vi/pAHwqloAb8w/maxresdefault.jpg",
+        title: "Caos e Mais Caos",
+        descri: "Esse é um curta de minecraft sobre um negociante que ganhou poderes e explodiu tudo.",
+        video: "pAHwqloAb8w"
+      },
+      id2: {
+        foto: "https://img.youtube.com/vi/fD9R7DNUEMA/maxresdefault.jpg",
+        title: "Dimensao da Caverna",
+        descri: "Esse é um curta de minecraft sobre um negociante que foi parar em uma dimensão com cavernas.",
+        video: "fD9R7DNUEMA"
+      },
+      id3: {
+        foto: "https://img.youtube.com/vi/7WJy9RhbTak/maxresdefault.jpg",
+        title: "A Guerra Jurassica",
+        descri: "Esse é um stop motion sobre um dinossauro que invadiu uma cidade.",
+        video: "7WJy9RhbTak"
+      }
+    };
+    
+
---
+++ listaSeries.js
@@ -0,0 +1,12 @@
+ let lista = {
+      id1: {
+        img: "https://img.youtube.com/vi/l-yQNK46qUI/maxresdefault.jpg",
+        quantidadeEp: 3,
+        titulo: "O Estranho da Terra",
+        descri: "Esse é um curta sobre um alien que teve o planeta destruido e foi para terra",
+        episodios: [
+          {nome: "Sozinho e Perdido", descri: "Um alien tem o planeta destruido", videoid: "l-yQNK46qUI"},
+          {nome: "Caindo na Floresta", descri: "O alien vai para a terra", videoid: "CLSnD2dwgT0"}
+        ]
+      }
+    };
