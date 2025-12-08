c84af159d75484ff316d9aea5bc87bfff36a8976
SupremeAlienCode <supremealiencode.contato@gmail.com>
2025-12-08 20:53:24 -0300

    Update listaMenu.js

--- listaMenu.js
+++ listaMenu.js
@@ -14,20 +14,20 @@ let lista = {
     id3: {
         title: "A Guerra Jurassica",
         img: "https://img.youtube.com/vi/7WJy9RhbTak/maxresdefault.jpg",
-        descri: "Esse é um stop motion sobre um dinossauro que invadiu uma cidade Ação Luiz Miguel ¿¿",
+        descri: "Esse é um stop motion sobre um dinossauro que invadiu uma cidade Ação Luiz Miguel",
         url: "filme.html?id=id3"
     },
     id4: {
         title: "O Estranho da Terra",
         img: "https://img.youtube.com/vi/l-yQNK46qUI/maxresdefault.jpg",
-        descri: "Esse é uma serie sobre um alien que teve o planeta destruido e foi para terra¿¿",
-        url: "series.html?id=id1"
+        descri: "Esse é uma serie sobre um alien que teve o planeta destruido e foi para terra",
+        url: "series.html?id=id4"
     }, 
     id5: {
-        title: "Teste 1",
-        img: "https://img.youtube.com/vi/l-yQNK46qUI/maxresdefault.jpg",
-        descri: "coco",
-        url: "filme.html?id=id3"
-    } 
+        title: "Matei o Pou",
+        img: "https://img.youtube.com/vi/TjBTFjEOLMI/maxresdefault.jpg",
+        descri: "Esse filme é um video de terror feito por Luiz Miguel¿¿",
+        url: "filme.html?id=id5"
+    }
 }
 
